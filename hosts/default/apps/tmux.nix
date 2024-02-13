b9c3963e54cebfef88d273866e057a7762ceb213
Abdulee <abdulee@disroot.org>
2024-02-13 15:14:42 +0100

    Tmux: added plugins

--- hosts/default/apps/tmux.nix
+++ hosts/default/apps/tmux.nix
@@ -1,6 +1,16 @@
-{pkgs, config, ...}:
+{pkgs, ...}:
 {
    programs.tmux = {
       enable = true;
+      terminal = "tmux-256color";
+      baseIndex = 1;
+      keyMode = "vi";
+      plugins = with pkgs; 
+      [
+         tmuxPlugins.gruvbox
+      ];
+      extraConfig = ''
+      set -g @tmux-gruvbox 'dark'
+      '';
    };
 }
